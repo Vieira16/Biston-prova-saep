41e6dc8ed0fed943c6aa26ef53d3601ad117d302
Vieira16 <rafaelcevieira8@gmail.com>
2026-02-12 09:10:35 -0300

    yout

--- youtube.js
+++ youtube.js
@@ -1,1 +1,43 @@
-console.log("FUNCIONOU");
+const { Builder, By, Key, until } = require('selenium-webdriver');
+
+async function abrirYoutube() {
+
+  let driver = await new Builder()
+    .forBrowser('chrome')
+    .build();
+
+  try {
+
+    // abrir youtube
+    await driver.get('https://www.youtube.com');
+
+    // esperar caixa de busca aparecer
+    let busca = await driver.wait(
+      until.elementLocated(By.name('search_query')),
+      10000
+    );
+
+    // digitar pesquisa
+    await busca.sendKeys('lofi music', Key.RETURN);
+
+    // esperar resultados carregarem
+    await driver.wait(
+      until.elementLocated(By.id('video-title')),
+      10000
+    );
+
+    // pegar primeiro vídeo
+    let primeiroVideo = await driver.findElement(By.id('video-title'));
+
+    // clicar
+    await primeiroVideo.click();
+
+    // esperar 10 segundos rodando
+    await driver.sleep(10000);
+
+  } finally {
+    await driver.quit();
+  }
+}
+
+abrirYoutube();
